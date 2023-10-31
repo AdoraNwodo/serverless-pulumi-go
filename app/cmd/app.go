40c6594e200cb19d0a8515f62d2a7dc575186fbb
Adora Nwodo <adoranwodo@Adoras-MacBook-Pro.local>
2023-10-31 19:04:44 +0000

    update functions

--- app/cmd/app.go
+++ app/cmd/app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -28,5 +29,38 @@ func main() {
 		w.Write([]byte(fmt.Sprintf(`{ "now": %d }`, time.Now().UnixNano()/1000000)))
 	})
 
+	mux.HandleFunc("/api/weather", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		weather := WeatherData{
+			City:        "New York",
+			Temperature: 72.5,
+			Condition:   "Partly Cloudy",
+		}
+
+		// Convert the weather data to JSON.
+		responseJSON, err := json.Marshal(weather)
+		if err != nil {
+			http.Error(w, "Error encoding response to JSON", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(responseJSON)
+	})
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", customHandlerPort), mux))
+}
+
+type WeatherData struct {
+	City        string  `json:"city"`
+	Temperature float64 `json:"temperature"`
+	Condition   string  `json:"condition"`
 }
